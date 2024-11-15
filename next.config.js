eda2af4520fe3c4556e6b41194cac257348716b9
tnynhi <truongnguyenyennhi.work@gmail.com>
2024-11-15 17:14:33 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  basePath: isProd ? '/nhitnyBlog.github.io' : '',
-  assetPrefix: isProd ? '/nhitnyBlog.github.io/' : '',
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
+    basePath: '/nhitnyBlog.github.io',
+    assetPrefix: '/nhitnyBlog.github.io/',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  

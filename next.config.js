cc4162709080561d4d8b254f282b84784ba56733
tnynhi <truongnguyenyennhi.work@gmail.com>
2024-11-15 16:39:20 +0700

    thêm file next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/nhitnyBlog.github.io',
+    assetPrefix: '/nhitnyBlog.github.io/',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
